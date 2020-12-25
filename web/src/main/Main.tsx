8d6712d02f8b73ba2028513b84ef9f6805b69a2c
kamilnaja <kamilnaja@gmail.com>
2020-12-25 01:31:00 +0100

    initial commit

---
+++ web/src/main/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../header/Header';
+
+function Main() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default Main;
