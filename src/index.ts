61f91e19ce954aa156dff260ee3b99ee0b8cd3af
kamilnaja <knaja@pentacomp.pl>
2021-01-09 18:42:45 +0100

    adding ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import route from './routes/route';
 import path from 'path';
 export const app = express();
 
-const port = process.env.PORT || 5000; // default port to listen
+const port = process.env.PORT || 8080; // default port to listen
 app.use(cors());
 app.use(express.json());
 
@@ -12,11 +12,11 @@ app.use(express.json());
 
 app.use('/api', route);
 
-// app.use(express.static(path.join(__dirname, '../web/build'))); // the same directory as below
+app.use(express.static(path.join(__dirname, '../../web/build'))); // the same directory as below
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../web/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../web/build/index.html'));
+});
 
 // start the Express server
 app.listen(port, () => {
