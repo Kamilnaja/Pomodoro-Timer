ffc761f057b8d7f36d49d7a7b6c48bd045b493c1
kamilnaja <knaja@pentacomp.pl>
2021-01-10 18:05:00 +0100

    vip

--- web/src/timer/containers/Timer.tsx
+++ web/src/timer/containers/Timer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { msToTime } from "../../shared/scripts/utils";
 import { Info } from "../components/info/Info";
-import { Time } from "../components/Time/Time";
+import { Time } from "../components/time/Time";
 import { TimerState } from "../store/enums/timer.enum";
 import { State } from "../store/interfaces/state.interface";
 import { TimerProps } from "../store/interfaces/timerProps.interface";
