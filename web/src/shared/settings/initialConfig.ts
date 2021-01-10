54cfd8846e3048c71cf2b8e5fe2a4a98a15fe11e
kamilnaja <kamilnaja@gmail.com>
2021-01-10 18:12:17 +0100

    changing url

--- web/src/shared/settings/initialConfig.ts
+++ web/src/shared/settings/initialConfig.ts
@@ -5,5 +5,5 @@ export const initialConfig: InitialConfig = {
   longBreakTime: 15 * 60 * 1000,
   pomodoroTime: 25 * 60 * 1000,
   refreshRate: 1000,
-  apiUrl: "http://localhost:8080/api",
+  apiUrl: "/api",
 };
