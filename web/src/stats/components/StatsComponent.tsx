7ad130b02ea082890270ee4b0a4257a4a15b526e
kamilnaja <knaja@pentacomp.pl>
2021-01-24 18:17:59 +0100

    adding stats table

--- web/src/stats/components/StatsComponent.tsx
+++ web/src/stats/components/StatsComponent.tsx
@@ -8,7 +8,23 @@ type StatsProps = {
 export const StatsComponent = (props: { stats: StatsProps }) => {
   return (
     <div className="stats">
-      <p>Today you have made: {props.stats.stats?.result[0]?.count} pomodoros</p>
+      <h3>Today you have made: {props.stats.stats?.result[0]?.count} pomodoros</h3>
+      <table className="stats__table">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Number of pomodoros</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.stats.stats?.result.map((v, idx) => (
+            <tr key={idx}>
+              <td>{v.date}</td>
+              <td>{v.count}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
