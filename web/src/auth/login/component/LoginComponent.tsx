068de5c9705978d8228ec2b47c3343b143560a7f
kamilnaja <knaja@pentacomp.pl>
2021-02-02 19:46:24 +0100

    fix for login component

--- web/src/auth/login/component/LoginComponent.tsx
+++ web/src/auth/login/component/LoginComponent.tsx
@@ -23,34 +23,46 @@ export const LoginComponent = (props: LoginProps) => {
 
   return (
     <div className="login">
-      <h2 className="login__header">Please login</h2>
-      {props.auth.error?.message && <ValidationMessage type={'error'} message={props.auth.error.message} />}
-      <form className="login__form form" onSubmit={handleSubmit(onSubmit)}>
-        <div className="form__row">
-          <label className="form__label">User login</label>
-          <input
-            className="form__input"
-            type="text"
-            name="login"
-            required
-            placeholder="John Doe"
-            ref={register({ required: true })}
-          />
-          <div className="form__error">
-            {errors.login && <ValidationMessage type={'error'} message={'😰Login is required'} />}
-          </div>
+      {props.auth.isSuccess ? (
+        <div className="login__success">
+          <ValidationMessage
+            type={'success'}
+            message="😀Login successfull, now close the modal and do some work!"
+          ></ValidationMessage>
         </div>
-        <div className="form__row">
-          <label className="form__label">Password</label>
-          <input type="password" name="password" required ref={register({ required: true })} />
-          <div className="form__error">
-            {errors.password && <ValidationMessage type={'error'} message={'😱Password is required'} />}
-          </div>
+      ) : (
+        <div>
+          <h2 className="login__header">Please login</h2>
+          {props.auth.error?.message && <ValidationMessage type={'error'} message={props.auth.error.message} />}
+          <form className="login__form form" onSubmit={handleSubmit(onSubmit)}>
+            <div className="form__row">
+              <label className="form__label">User login</label>
+              <input
+                className="form__input"
+                type="text"
+                name="login"
+                required
+                placeholder="John Doe"
+                ref={register({ required: true })}
+                autoFocus
+              />
+              <div className="form__error">
+                {errors.login && <ValidationMessage type={'error'} message={'😰Login is required'} />}
+              </div>
+            </div>
+            <div className="form__row">
+              <label className="form__label">Password</label>
+              <input type="password" name="password" required ref={register({ required: true })} />
+              <div className="form__error">
+                {errors.password && <ValidationMessage type={'error'} message={'😱Password is required'} />}
+              </div>
+            </div>
+            <button className="form__button" value="Wyślij" type="submit">
+              Submit
+            </button>
+          </form>
         </div>
-        <button className="form__button" value="Wyślij" type="submit">
-          Submit
-        </button>
-      </form>
+      )}
     </div>
   );
 };
