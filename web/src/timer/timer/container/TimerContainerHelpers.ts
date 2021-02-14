d29f4697776239d0bb60c70011496873b4eeabc4
kamilnaja <kamilnaja@gmail.com>
2021-02-14 21:35:40 +0100

    dealing with new timer

---
+++ web/src/timer/timer/container/TimerContainerHelpers.ts
@@ -0,0 +1,9 @@
+export const playClickSound = () => {
+  const audio = new Audio('sounds/zapsplat_multimedia_game_sound_childrens_ping_high_pitched_soft_007_60676.mp3');
+  audio.play();
+};
+
+export const playEndSound = () => {
+  const audio = new Audio('sounds/zapsplat_multimedia_game_sound_positive_award_bonus_bright_warm_synth_001_60698.mp3');
+  // audio.play();
+};
