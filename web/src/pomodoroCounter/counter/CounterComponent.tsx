6947f85c2cd4f18d513be8f5e60cb5d9044b3525
kamilnaja <knaja@pentacomp.pl>
2021-02-16 16:50:47 +0100

    removing time

--- web/src/pomodoroCounter/counter/CounterComponent.tsx
+++ web/src/pomodoroCounter/counter/CounterComponent.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { CounterComponentProps } from './CounterComponentProps';
-import { TimeComponent } from '../time/TimeComponent';
 import { isAnyTimerRunning } from '../container/PomodoroCounterContainerHelpers';
+import './counterComponent.scss';
 
 export const CounterComponent = (props: CounterComponentProps) => {
   return (
     <>
-      <TimeComponent time={props.time} />
+      <span className="time text-light d-flex align-items-center">{props.time}</span>
       <Button variant="success" onClick={isAnyTimerRunning(props.state) ? props.pauseCounter : props.startCounter}>
         {isAnyTimerRunning(props.state) ? 'Stop timer' : 'Start timer'}
       </Button>
--- web/src/pomodoroCounter/time/TimeComponent.tsx
+++
@@ -1,6 +0,0 @@
-import './time.scss';
-import { TimeComponentProps } from './TimeComponentProps';
-
-export const TimeComponent = (props: TimeComponentProps) => {
-  return <div className="time text-light d-flex align-items-center">{props.time}</div>;
-};
