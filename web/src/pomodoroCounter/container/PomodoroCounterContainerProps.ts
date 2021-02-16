0636a839f6295286dc23c511ad41ecbbf59e53c2
kamilnaja <kamilnaja@gmail.com>
2021-02-16 20:57:01 +0100

    moving counter state to redux

--- web/src/pomodoroCounter/container/PomodoroCounterContainerProps.ts
+++ web/src/pomodoroCounter/container/PomodoroCounterContainerProps.ts
@@ -1,6 +1,15 @@
 import { AuthState } from '../../auth/store/interfaces/authState';
+import { PomodoroCounterActions } from '../store/actions/pomodoroCounterAction';
+import { PomodoroCounterState } from '../store/interfaces/PomodoroCounterState';
 
 export interface PomodoroCounterContainerProps {
   handleSavePomodoro: () => void;
   auth: AuthState;
+  pomodoroCounter: PomodoroCounterState;
+  pomodoroRun: () => void;
+  breakRun: () => void;
+  pomodoroPause: () => void;
+  breakPause: () => void;
+  breakEnd: () => void;
+  pomodoroEnd: () => void;
 }
--- web/src/pomodoroCounter/store/actions/pomodoroCounterAction.ts
+++ web/src/pomodoroCounter/store/actions/pomodoroCounterAction.ts
@@ -1,9 +1,34 @@
 import { Action } from 'redux';
 
 export enum PomodoroCounterActions {
-  START_COUNTER = 'START_COUNTER',
+  POMODORO_PAUSE = 'POMODORO_PAUSE',
+  BREAK_PAUSE = 'BREAK_PAUSE',
+  POMODORO_RUN = 'POMODORO_RUN',
+  BREAK_RUN = 'BREAK_RUN',
+  BREAK_END = 'BREAK_END',
+  POMODORO_END = 'POMODORO_END',
 }
 
-export const startCounter = (): Action<PomodoroCounterActions> => ({
-  type: PomodoroCounterActions.START_COUNTER,
+export const pomodoroPause = (): Action<PomodoroCounterActions> => ({
+  type: PomodoroCounterActions.POMODORO_PAUSE,
+});
+
+export const breakPause = (): Action<PomodoroCounterActions> => ({
+  type: PomodoroCounterActions.BREAK_PAUSE,
+});
+
+export const pomodoroRun = (): Action<PomodoroCounterActions> => ({
+  type: PomodoroCounterActions.POMODORO_RUN,
+});
+
+export const pomodoroEnd = (): Action<PomodoroCounterActions> => ({
+  type: PomodoroCounterActions.POMODORO_END,
+});
+
+export const breakRun = (): Action<PomodoroCounterActions> => ({
+  type: PomodoroCounterActions.BREAK_RUN,
+});
+
+export const breakEnd = (): Action<PomodoroCounterActions> => ({
+  type: PomodoroCounterActions.BREAK_END,
 });
