820fb6852fcc8112c98a59121f60a9639de19222
kamilnaja <knaja@pentacomp.pl>
2021-02-28 16:51:09 +0100

    stats table

--- web/src/stats/allStats/statsTable/container/StatsTableContainer.tsx
+++ web/src/stats/allStats/statsTable/container/StatsTableContainer.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { StatsTableComponent } from '../component/StatsTableComponent';
 import { StatsTableContainerProps } from './StatsTableContainerProps';
+import { StatsTableContainerState } from './StatsTableContainerState';
 
-export class StatsTableContainer extends React.Component<StatsTableContainerProps> {
+export class StatsTableContainer extends React.Component<StatsTableContainerProps, StatsTableContainerState> {
+  constructor(props: StatsTableContainerProps) {
+    super(props);
+    this.state = {
+      sortAscending: false,
+      showEmptyDays: false,
+    };
+  }
+
+  toggleSorting = () =>
+    this.setState({
+      sortAscending: !this.state.sortAscending,
+    });
+
   render() {
     return (
       <StatsTableComponent
---
+++ web/src/stats/allStats/statsTable/container/StatsTableContainerState.ts
@@ -0,0 +1,4 @@
+export interface StatsTableContainerState {
+  sortAscending: boolean;
+  showEmptyDays: boolean;
+}
