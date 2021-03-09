d5133d1be9e74a36c7a4ff79550fdaedf788cbb1
kamilnaja <knaja@pentacomp.pl>
2021-03-09 22:02:31 +0100

    adding simple table

--- web/src/stats/statsWithTags/components/StatsWithTagsComponent.tsx
+++ web/src/stats/statsWithTags/components/StatsWithTagsComponent.tsx
@@ -1,5 +1,26 @@
 import { StatsWithTagsComponentProps } from './StatsWithTagsComponentProps';
+import { Table } from 'react-bootstrap';
+import './StatsWithTagsComponent.scss';
 
 export const StatsWithTagsComponent = (props: StatsWithTagsComponentProps) => {
-  return <div>Hello stats with</div>;
+  return (
+    <div className="table">
+      <Table striped bordered hover className="stats__table table" size="sm">
+        <thead className="table__head">
+          <tr>
+            <th>Day</th>
+            <th>Tags</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.statsWithTags.result.map((v, idx) => (
+            <tr key={idx}>
+              <td>{v.createdAt}</td>
+              <td>{v.tagText}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
