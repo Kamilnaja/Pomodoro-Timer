64d92f8c3ca923f9ea10ead9a0da5992efcee676
kamilnaja <knaja@pentacomp.pl>
2021-03-31 07:05:35 +0200

    fix for wrong date

--- server/src/utils/service.util.ts
+++ server/src/utils/service.util.ts
@@ -8,4 +8,4 @@ export const isDateError = (year: string, month: string, day?: string): boolean 
 
 export const isYearCorrect = (year: string): boolean => Number(year) >= 2020;
 export const isMonthCorrect = (month: string): boolean => Number(month) >= 0 && Number(month) <= 12;
-export const isDayCorrect = (day: string): boolean => Number(day) >= 0 && Number(day) < 31;
+export const isDayCorrect = (day: string): boolean => Number(day) >= 0 && Number(day) <= 31;
