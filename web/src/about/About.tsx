0d658a78346c1c4e1c63a2917680a732400e3c68
kamilnaja <knaja@pentacomp.pl>
2021-07-04 14:27:39 +0200

    fix for css

--- web/src/about/About.tsx
+++ web/src/about/About.tsx
@@ -1,8 +1,9 @@
 import { Container } from 'react-bootstrap';
+import './about.scss';
 
 export const About = () => {
   return (
-    <Container>
+    <Container className="about">
       <h2>About</h2>
       <h3>What is this page made for?</h3>
       <p>TomatoToDone is an pomodoro application, that helps you achieve more in live and work.</p>
